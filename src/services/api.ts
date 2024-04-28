9535d75b1091e08eadc063c2acbd3d7e35526637
Lonast <aaalonast@gmail.com>
2024-04-28 13:25:55 +0300

    Project created

--- src/services/api.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import { ITodo } from "../types/types";
-
-const BASE_URL = "https://jsonplaceholder.typicode.com/todos/";
-const axiosInstance = axios.create({ baseURL: BASE_URL });
-
-export const getTodos = async () => {
-  return (await axios.get<ITodo[]>("todos")).data;
-};
